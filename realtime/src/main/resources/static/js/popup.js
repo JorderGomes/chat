b46a01f730bbb65f6d1cca413492e890cd61563a
JorderGomes <jordernoveau@gmail.com>
2024-01-05 12:41:28 -0300

    add backend

---
+++ realtime/src/main/resources/static/js/popup.js
@@ -0,0 +1,20 @@
+function openPopup() {
+    const popup = document.getElementById("popup");
+    popup.classList.remove("hidden");
+}
+
+function openChat() {
+    const popup = document.getElementById("popup");
+    const chatContainer = document.getElementById("chatContainer");
+    const usernameInput = document.getElementById("usernameInput").value;
+
+    if (usernameInput !== "") {
+        popup.style.display = "none";
+        chatContainer.classList.remove("hidden");
+        sessionStorage.setItem("user", usernameInput);
+    } else {
+        alert("Digite um nome válido!");
+    }
+}
+
+openPopup();
